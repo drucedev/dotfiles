6ed5a93c4992bfec088390d925df9ff7065c343c
Andrei Kukharau <contact@druce.dev>
2024-08-30 15:20:25 +0200

    Changed theme to Tokio Night

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,9 @@ config.font_size = 18
 
 config.enable_tab_bar = false
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Tokyo Night'
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 0
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 5
 
 return config
