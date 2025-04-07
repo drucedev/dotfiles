4bf3c581b5dfae40534ab6c579620103bd58d492
Andrei Kukharau <contact@druce.dev>
2025-04-07 09:51:32 +0200

    Enable tabs in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,8 +10,6 @@ local config = wezterm.config_builder()
 
 config.font_size = 16
 
-config.enable_tab_bar = false
-
 config.color_scheme = 'Catppuccin Mocha'
 
 config.window_background_opacity = 0.85
