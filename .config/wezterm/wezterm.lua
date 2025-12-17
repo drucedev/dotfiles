a07b9b98ae582827d4006a9cee4e39209ba04a82
Andrei Kukharau <contact@druce.dev>
2025-12-17 16:20:10 +0100

    Update wezterm.lua with claude-code related config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,4 +20,8 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
+config.keys = {
+    {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+}
+
 return config
